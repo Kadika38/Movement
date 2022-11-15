db6a96371722df7a1a9fbc4d601fde27d4880cc8
Kadika <dryan10101@gmail.com>
2022-11-14 18:14:29 -0800

    turned board into a randomly set up board of 'wall' squares and non-'wall' squares

--- client/src/components/Board/Board.js
+++ client/src/components/Board/Board.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Square from "./components/Square/Square";
 import "./style.css";
 
@@ -7,62 +7,29 @@ export default function Board() {
     const wide = 10;
     const high = 10;
     const mapper = [];
-    const finalMapper = [];
-    const startEmpty = [];
     for (let i = 0; i < high; i++) {
-        mapper.push(startEmpty);
+        mapper.push([]);
     }
 
     const randBool = () => {
         return (Math.random() >= 0.5);
     };
 
-    //create the first square entirely randomly
-    //all sqaures are [left, top, right, bottom]
-    mapper[0].push([true, true, randBool(), randBool()]);
-    finalMapper.push(mapper[0][0]);
-
-    //add in the rest based on the first square
-    for (let y = 0; y < high; y++) {
-        for (let x = 0; x < wide; x++) {
-            let holder = [];
-            if (y > 0 || x > 0) {
-                //check sides
-                //left
-                if (x > 0) { //if not on left side of board, copy the right border of the square to the left of the current square
-                    holder.push(mapper[y][x-1][2]);
-                } else {
-                    holder.push(true);
-                }
-                //top
-                if (y > 0) { //if not on top of board, copy the bottom border of the square above the current square
-                    holder.push(mapper[y-1][x][3]);
-                } else {
-                    holder.push(true);
-                }
-                //right
-                if (x < wide-1) { //if not on right side of board, random border
-                    holder.push(randBool());
-                } else {
-                    holder.push(true);
-                }
-                //bottom
-                if (y > high-1) { //if not on bottom of board, random border
-                    holder.push(randBool());
-                } else {
-                    holder.push(true);
-                }
-                mapper[y][x] = holder;
-                finalMapper.push(holder);
-            }
+    for (let i = 0; i < high; i++) {
+        for (let j = 0; j < wide; j++) {
+            (i === 0 && j === 0) ? mapper[i].push(false) : mapper[i].push(randBool());
         }
     }
 
     return (
         <div className="board-outer">
-            {finalMapper.map((sq) => {
+            {mapper.map((row) => {
                 return (
-                    <Square left={sq[0]} top={sq[1]} right={sq[2]} bottom={sq[3]}></Square>
+                    row.map((sq) => {
+                        return (
+                            <Square wall={sq}></Square>
+                        )
+                    })
                 )
             })}
         </div>
--- client/src/components/Board/components/Square/Square.js
+++ client/src/components/Board/components/Square/Square.js
@@ -1,14 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import "../../style.css";
 
 export default function Square(props) {
-    const width = 30 - (props.left ? 1 : 0) - (props.right ? 1 : 0);
-    const height = 30 - (props.top ? 1 : 0) - (props.bottom ? 1 : 0);
-
-    const singular = (props.left && props.right && props.top && props.bottom);
 
     return (
-        <div style={{borderLeft:(props.left ? "solid" : "none"), borderTop:(props.top ? "solid" : "none"), borderRight:(props.right ? "solid" : "none"), borderBottom:(props.bottom ? "solid" : "none"), borderWidth:"1px", width:(width === 30 ? "30px" : (width === 29 ? "29px" : "28px")), height:(height === 30 ? "30px" : (height === 29 ? "29px" : "28px")), backgroundColor:(singular ? "black" : "white")}}>
+        <div style={{borderLeft:"solid", borderTop:"solid", borderRight:"solid", borderBottom:"solid", borderWidth:"1px", width:"28px", height:"28px", backgroundColor:(props.wall ? "black" : "white")}}>
             
         </div>
     );
