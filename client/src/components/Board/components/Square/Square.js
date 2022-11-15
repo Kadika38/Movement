76bc8ae8d14db8903ed037ea6b28152997197dc7
Kadika <dryan10101@gmail.com>
2022-11-14 17:03:08 -0800

    added black background to 1x1 squares on board

--- client/src/components/Board/components/Square/Square.js
+++ client/src/components/Board/components/Square/Square.js
@@ -5,8 +5,10 @@ export default function Square(props) {
     const width = 30 - (props.left ? 1 : 0) - (props.right ? 1 : 0);
     const height = 30 - (props.top ? 1 : 0) - (props.bottom ? 1 : 0);
 
+    const singular = (props.left && props.right && props.top && props.bottom);
+
     return (
-        <div style={{borderLeft:(props.left ? "solid" : "none"), borderTop:(props.top ? "solid" : "none"), borderRight:(props.right ? "solid" : "none"), borderBottom:(props.bottom ? "solid" : "none"), borderWidth:"1px", width:(width === 30 ? "30px" : (width === 29 ? "29px" : "28px")), height:(height === 30 ? "30px" : (height === 29 ? "29px" : "28px"))}}>
+        <div style={{borderLeft:(props.left ? "solid" : "none"), borderTop:(props.top ? "solid" : "none"), borderRight:(props.right ? "solid" : "none"), borderBottom:(props.bottom ? "solid" : "none"), borderWidth:"1px", width:(width === 30 ? "30px" : (width === 29 ? "29px" : "28px")), height:(height === 30 ? "30px" : (height === 29 ? "29px" : "28px")), backgroundColor:(singular ? "black" : "white")}}>
             
         </div>
     );
